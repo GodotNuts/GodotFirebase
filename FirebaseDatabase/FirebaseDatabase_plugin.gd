31aca6a7b7f8c6ec336495d0843e09c0be507782
Kyle Szklenski <kyle.szklenski@gmail.com>
2019-02-09 08:05:25 -0500

    Update FirebaseDatabase_plugin.gd

--- FirebaseDatabase/FirebaseDatabase_plugin.gd
+++ FirebaseDatabase/FirebaseDatabase_plugin.gd
@@ -3,7 +3,6 @@ extends EditorPlugin
 
 func _enter_tree():
     add_autoload_singleton("FirebaseDatabase", "res://addons/FirebaseDatabase/FirebaseDatabase.gd")
-#    add_custom_type("FirebaseDatabase", "Node", preload("FirebaseDatabase.gd"), preload("AuthIcon.png"))
 
 func _exit_tree():
     remove_autoload_singleton("FirebaseDatabase")
