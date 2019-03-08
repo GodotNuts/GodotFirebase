254c30c60cfb3767efb1a35b24ef84a89858b0ea
Kyle Szklenski <kyle.szklenski@gmail.com>
2019-03-08 06:53:19 -0500

    Small updates

--- GDFirebase/FirebaseDatabase.gd
+++ GDFirebase/FirebaseDatabase.gd
@@ -20,7 +20,7 @@ const EqualTo = "equalTo"
 func _on_FirebaseAuth_login_succeeded(auth_result):
     auth = auth_result
 
-func get_database_reference(path : String, filter : Dictionary):        
+func get_filtered_database_reference(path : String, filter : Dictionary):        
     var firebase_reference = Node.new()
     firebase_reference.set_script(load("res://addons/GDFirebase/FirebaseReference.gd"))
     var pusher = HTTPRequest.new()
@@ -33,3 +33,7 @@ func get_database_reference(path : String, filter : Dictionary):
     firebase_reference.set_db_path(path, filter)
     add_child(firebase_reference)
     return firebase_reference
+
+
+func get_database_reference(path : String):
+    return get_filtered_database_reference(path, {})
