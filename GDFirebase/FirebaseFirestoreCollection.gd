1fe6a4f0ffa12a8dcf14e59c1be2a8852355070c
Nicolò Santilio <n.santilio97@gmail.com>
2020-06-05 17:15:46 +0200

    Updates and Fix
    *FirebaseFirestore.gd*   
    - fixed `list()` function   
       
    *FirebaseFirestoreCollection.gd*   
    - classified with `FirestoreCollection` to create one in another script. It is useful, since multiple calls on the 
    same collection would always require to run FirestoreCollection.gd and use a long line of code. A FirestoreCollection class can be used like: `var clients_collection : FirestoreCollection = FirebaseFirestore.collection("clients")`   
    - added `get()`, `update()` and `delete()` functions to expand APIs on Firestore Collections
    - added signals linked to functions
    - fixed HTTPRequests
    
    *FirebaseFirestoreDocument.gd*   
    - classified with `FirestoreDocument` to create one in another script. It can be used to use REST responses as objects, containing all datas needed. 
    - added `dict2fields()` to parse a Dictionary of values to a Fields object required by REST calls on Collections/Documents (ex. `dict2fields({ 'name':'MyName' }) -> { 'fields' : { 'stringValue' : 'MyName' } }`)   
    - added `fields2dict()` to parse a FirestoreDocument content (which is the direct response of a document request) to a dictionary (ex. `fields2dict({ 'fields' : { 'stringValue' : 'MyName' } }) -> { 'name':'MyName' }`)

--- GDFirebase/FirebaseFirestoreCollection.gd
+++ GDFirebase/FirebaseFirestoreCollection.gd
@@ -1,6 +1,11 @@
 extends Node
 
-signal new_document(doc)
+class_name FirestoreCollection
+
+signal add_document(doc)
+signal get_document(doc)
+signal update_document(doc)
+signal delete_document()
 
 var base_url
 var extended_url
@@ -28,44 +33,86 @@ const equal_tag = "="
 const key_filter_tag = "$key"
 const documentId_tag = "documentId="
 
+var request : int
+enum REQUESTS {
+	ADD,
+	GET,
+	UPDATE,
+	DELETE
+}
+
 func _ready():
-    var push_node = HTTPRequest.new()
-    add_child(push_node)
-    pusher = push_node
-    pusher.connect("request_completed", self, "on_pusher_request_complete")
+	var push_node = HTTPRequest.new()
+	add_child(push_node)
+	pusher = push_node
+	pusher.connect("request_completed", self, "on_pusher_request_complete")
 
-func add(documentId = null, data = null):
-    if auth:
-        var url = _get_request_url()
-        if !data:
-            data = build_fake_data()
-        
-        if documentId:
-            url += query_tag + documentId_tag + documentId
-        
-        
-        pusher.request(url, [authorization_header + auth.idtoken], true, HTTPClient.METHOD_POST, JSON.print(data))
-        pass
+# ----------------------- REQUESTS
 
-func build_fake_data():
-    var packed_scene = PackedScene.new()
-    packed_scene.pack(get_tree().current_scene)
-    var scene_text = var2str(packed_scene)
-    return {"fields": { "scene_text": {"stringValue": "garbage" } } }
+# used to SAVE/ADD a new document to the collection, specify @documentID and @fields
+func add(documentId : String, fields : Dictionary = {}):
+	if auth:
+		request = REQUESTS.ADD
+		var url = _get_request_url()
+		url += query_tag + documentId_tag + documentId
+		
+		pusher.request(url, [authorization_header + auth.idtoken], true, HTTPClient.METHOD_POST, JSON.print(fields))
+	else:
+		printerr("Unauthorized")
 
+# used to GET a document from the collection, specify @documentId
+func get(documentId : String):
+	if auth:
+		request = REQUESTS.GET
+		var url = _get_request_url() + separator + documentId
+		
+		pusher.request(url, [authorization_header + auth.idtoken], true, HTTPClient.METHOD_GET)
+	else:
+		printerr("Unauthorized")
+
+# used to UPDATE a document, specify @documentID and @fields
+func update(documentId : String, fields : Dictionary = {}):
+	if auth:
+		request = REQUESTS.UPDATE
+		var url = _get_request_url() + separator + documentId
+		print(fields)
+		pusher.request(url, [authorization_header + auth.idtoken], true, HTTPClient.METHOD_PATCH, JSON.print(fields))
+	else:
+		printerr("Unauthorized")
+
+# used to DELETE a document, specify @documentId
+func delete(documentId : String):
+	if auth:
+		request = REQUESTS.DELETE
+		var url = _get_request_url() + separator + documentId
+		
+		pusher.request(url, [authorization_header + auth.idtoken], true, HTTPClient.METHOD_DELETE)
+	else:
+		printerr("Unauthorized")
+
+# ----------------- Functions
 func _get_request_url():
-    return base_url + extended_url + collection_name
-    pass
-    
-func doc(documentId = "", data = null):
-    pass
+	return base_url + extended_url + collection_name
 
 
+# ---------------- RESPONSES
 func on_pusher_request_complete(result, response_code, headers, body):
-    var bod = body.get_string_from_utf8()
-    if response_code == HTTPClient.RESPONSE_OK:
-        emit_signal("new_document", JSON.parse(bod))
-        pass
-    else:
-        print(bod)
-    pass
+	var bod = body.get_string_from_utf8()
+	if response_code == HTTPClient.RESPONSE_OK:
+		match request:
+			REQUESTS.ADD:	
+				var doc_infos : Dictionary = JSON.parse(bod).result
+				var document : FirestoreDocument = FirestoreDocument.new(doc_infos, doc_infos.name, doc_infos.fields) 
+				emit_signal("add_document", document )
+			REQUESTS.GET:
+				var doc_infos : Dictionary = JSON.parse(bod).result
+				var document : FirestoreDocument = FirestoreDocument.new(doc_infos, doc_infos.name, doc_infos.fields) 
+				emit_signal("get_document", document )
+			REQUESTS.UPDATE:
+				var doc_infos : Dictionary = JSON.parse(bod).result
+				var document : FirestoreDocument = FirestoreDocument.new(doc_infos, doc_infos.name, doc_infos.fields) 
+				emit_signal("update_document", document )
+			REQUESTS.DELETE:
+				emit_signal("delete_document")
+	else:
+		print(bod)
