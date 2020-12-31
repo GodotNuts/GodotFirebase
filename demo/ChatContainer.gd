8ab9f712a03ca53828ff23bc1c718af62a2a26be
Luke Hollenback <8144622+lukehollenback@users.noreply.github.com>
2020-12-31 14:49:07 -0700

    Changed demo to use a soft-link to addon (easier to maintain).

--- demo/ChatContainer.gd
+++ demo/ChatContainer.gd
@@ -1,6 +1,6 @@
-extends "res://addons/GDFirebase/FirebaseContainer.gd"
+extends "res://addons/godot-firebase/FirebaseContainer.gd"
 
 signal chat_added
 
 func on_item_added(item, key, template):
-    emit_signal("chat_added")
+	emit_signal("chat_added")
