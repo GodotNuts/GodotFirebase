c57fc8a4b62c49cbb08455ae50b654a6a1c13805
Nicolò Santilio <n.santilio97@gmail.com>
2021-02-02 20:26:40 +0100

    fix firestore update call

--- addons/godot-firebase/firestore/firestore_collection.gd
+++ addons/godot-firebase/firestore/firestore_collection.gd
@@ -87,7 +87,10 @@ func update(documentId : String, fields : Dictionary = {}) -> void:
     if auth:
         if is_pusher_available([REQUESTS.UPDATE, documentId, fields]):
             request = REQUESTS.UPDATE
-            var url = _get_request_url() + separator + documentId.replace(" ", "%20")
+			var url = _get_request_url() + separator + documentId.replace(" ", "%20") + "?"
+            for key in fields.keys():
+                url+="updateMask.fieldPaths={key}&".format({key = key})
+            url = url.rstrip("&")
             pusher.request(url, [authorization_header + auth.idtoken], true, HTTPClient.METHOD_PATCH, JSON.print(FirestoreDocument.dict2fields(fields)))
     else:
         printerr("Unauthorized")
