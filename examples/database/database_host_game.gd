cf23dd1fcc113b388c1d4a9edea88e11244ab464
Tim Robinson <tim@timjrobinson.com>
2021-02-15 18:21:49 +1000

    Add Database and Firestore examples
    - Add simple host game examples using the realtime database and
      Firestore

---
+++ examples/database/database_host_game.gd
@@ -0,0 +1,46 @@
+extends Node
+
+var db_ref : FirebaseDatabaseReference 
+
+signal authenticated()
+signal room_updated(info)
+
+func _ready():
+	Firebase.Auth.connect("signup_succeeded", self, "_create_game")
+	Firebase.Auth.connect("login_succeeded", self, "_create_game")
+	Firebase.Auth.connect("login_failed", self, "_on_auth_error")
+	Firebase.Auth.login_anonymous()
+
+func _create_room_code(length):
+	randomize()
+	var result           = ''	
+	var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	var charactersLength = characters.length()
+	for _i in range(length):
+		result += characters[floor(rand_range(0, charactersLength))]
+	return result;
+
+func _create_game(auth_result : Dictionary):
+	emit_signal("authenticated")
+	var room_code = _create_room_code(4)
+		
+	var rooms_ref : FirebaseDatabaseReference = Firebase.Database.get_database_reference("/rooms/", {})
+	rooms_ref.connect("push_successful", self, "_connect_to_room", [room_code])
+	
+	rooms_ref.update(room_code, {
+		"code": room_code,
+	})
+	
+func _connect_to_room(room_code):
+	db_ref = Firebase.Database.get_database_reference("/rooms/" + room_code, {})
+	db_ref.connect("new_data_update", self, "_updated_data")
+	db_ref.connect("patch_data_update", self, "_updated_data")
+	db_ref.connect("push_successful", self, "_push_successful")
+	db_ref.connect("push_failed", self, "_push_failed")
+
+func _updated_data(data):
+	var info = db_ref.get_data()
+	emit_signal("room_updated", info)
+	
+func _on_auth_error(code, msg):
+	print("Authentication errror encountered. Code: ", code, " MSG: ", msg)
---
+++ examples/firestore/firestore_host_game.gd
@@ -0,0 +1,55 @@
+extends Node
+
+var ROOMS_DATABASE = "rooms"
+
+var collection
+var room_code
+
+onready var timer = $Timer
+
+signal authenticated()
+signal room_updated(info)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Firebase.Auth.connect("signup_succeeded", self, "_create_game")
+	Firebase.Auth.connect("login_succeeded", self, "_create_game")
+	Firebase.Auth.connect("login_failed", self, "_on_auth_error")
+	Firebase.Auth.login_anonymous()
+
+func _create_room_code(length):
+	randomize()
+	var result           = ''	
+	var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	var charactersLength = characters.length()
+	for _i in range(length):
+		result += characters[floor(rand_range(0, charactersLength))]
+	return result;
+
+func _create_game(auth_result : Dictionary):
+	emit_signal("authenticated")
+	
+	collection = Firebase.Firestore.collection(ROOMS_DATABASE)
+	collection.connect("error", self, "_on_network_error")
+	
+	room_code = _create_room_code(4)
+	var room_information = {
+		"code": room_code,
+		"players": []
+	}
+	collection.add(room_code, room_information)
+	var document : FirestoreDocument = yield(collection, "add_document")	
+	
+	timer.start()
+
+func _on_auth_error(code, msg):
+	print("Authentication errror encountered. Code: ", code, " MSG: ", msg)
+
+func _on_network_error(code, status, msg):
+	print("Network error encountered. Code: ", code, " Status: ", status, " MSG: ", msg)
+
+func _on_Timer_timeout():
+	collection.get(room_code)
+	var document : FirestoreDocument = yield(collection, "get_document")
+	var room_information = document['doc_fields']
+	emit_signal("room_updated", room_information)
