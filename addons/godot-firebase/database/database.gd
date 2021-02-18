7fcfd9b0751dfc8a9e4b7137e0f1b19cbebe6b2c
Kyle Szklenski <kyle.szklenski@gmail.com>
2021-02-18 13:34:14 -0500

    Fix auth variables to _auth

--- addons/godot-firebase/database/database.gd
+++ addons/godot-firebase/database/database.gd
@@ -13,21 +13,21 @@ const START_AT : String = "startAt"
 const END_AT : String = "endAt"
 const EQUAL_TO : String = "equalTo"
 
-var config : Dictionary = {}
+var _config : Dictionary = {}
 
-var auth : Dictionary = {}
+var _auth : Dictionary = {}
 
 func _set_config(config_json : Dictionary) -> void:
-    config = config_json
+    _config = config_json
 
 func _on_FirebaseAuth_login_succeeded(auth_result : Dictionary) -> void:
-    auth = auth_result
+    _auth = auth_result
 
 func _on_FirebaseAuth_token_refresh_succeeded(auth_result : Dictionary) -> void:
-    auth = auth_result
+    _auth = auth_result
 
 func _on_FirebaseAuth_logout() -> void:
-    auth = {}
+    _auth = {}
 
 func get_database_reference(path : String, filter : Dictionary = {}) -> FirebaseDatabaseReference:
     var firebase_reference : FirebaseDatabaseReference = FirebaseDatabaseReference.new()
@@ -36,7 +36,7 @@ func get_database_reference(path : String, filter : Dictionary = {}) -> Firebase
     listener.set_script(load("res://addons/http-sse-client/HTTPSSEClient.gd"))
     var store : FirebaseDatabaseStore = FirebaseDatabaseStore.new()
     firebase_reference.set_db_path(path, filter)
-    firebase_reference.set_auth_and_config(auth, config)
+    firebase_reference.set_auth_and_config(_auth, _config)
     firebase_reference.set_pusher(pusher)
     firebase_reference.set_listener(listener)
     firebase_reference.set_store(store)
