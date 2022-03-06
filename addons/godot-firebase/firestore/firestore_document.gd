25c492b371fca6dbd97a2607c8f7f4ce7316a9a5
fenix-hub <n.santilio97@gmail.com>
2022-03-06 02:50:12 +0100

    fix dict2fields building

--- addons/godot-firebase/firestore/firestore_document.gd
+++ addons/godot-firebase/firestore/firestore_document.gd
@@ -54,7 +54,11 @@ static func dict2fields(dict : Dictionary) -> Dictionary:
             TYPE_ARRAY:
                 var_type = "arrayValue"
                 field_value = {"values": array2fields(field_value)}
-        fields[field] = { var_type : field_value }
+        if fields.has(field) and fields[field].has("mapValue") and field_value.has("fields"):
+            for key in field_value["fields"].keys():
+                fields[field]["mapValue"]["fields"][key] = field_value["fields"][key]
+        else:
+            fields[field] = { var_type : field_value }
     return {'fields' : fields}
 
 # Pass the .fields inside a Firestore Document to print out the Dictionary { 'key' : 'value' }
