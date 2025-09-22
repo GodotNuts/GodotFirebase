88b43011bb6524bbe4602892d70de83955f74ce4
Lotof76 <gyorgy.molnar@gmail.com>
2025-09-22 11:09:37 +0200

    Fixing https://github.com/GodotNuts/GodotFirebase/issues/474 (#489)
    Adding new value "databaseName" to config and replacing the database name in the _extended_url variable. Also defaulting it to "(default)" to preserve it's previous behaviour.

--- addons/godot-firebase/firestore/firestore.gd
+++ addons/godot-firebase/firestore/firestore.gd
@@ -52,7 +52,7 @@ var _encrypt_key := "5vg76n90345f7w390346" if Utilities.is_web() else OS.get_uni
 
 
 var _base_url: String = ""
-var _extended_url: String = "projects/[PROJECT_ID]/databases/(default)/documents/"
+var _extended_url: String = "projects/{PROJECT_ID}/databases/{DATABASE_NAME}/documents/"
 var _query_suffix: String = ":runQuery"
 var _agg_query_suffix: String = ":runAggregationQuery"
 
@@ -171,8 +171,9 @@ func list(path : String = "", page_size : int = 0, page_token : String = "", ord
 func _set_config(config_json : Dictionary) -> void:
 	_config = config_json
 	_cache_loc = _config["cacheLocation"]
-	_extended_url = _extended_url.replace("[PROJECT_ID]", _config.projectId)
-
+	if _config.databaseName.is_empty():
+		_config.databaseName = "(default)"
+	_extended_url = _extended_url.format({"PROJECT_ID":_config.projectId, "DATABASE_NAME":_config.databaseName})
 	# Since caching is causing a lot of issues, I'm removing this check for now. We will revisit this in the future, once we have some time to investigate why the cache is being corrupted.
 
 	_check_emulating()
