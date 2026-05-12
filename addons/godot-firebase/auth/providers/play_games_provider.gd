0fdff297f40fd7afa21905a57671540bbf192e76
Rohan Choudhary <rohan5210work@gmail.com>
2026-05-12 11:17:38 +0530

    Add return type to _init function in PlayGamesProvider

--- addons/godot-firebase/auth/providers/play_games_provider.gd
+++ addons/godot-firebase/auth/providers/play_games_provider.gd
@@ -1,6 +1,7 @@
+# auth/providers/play_games_provider.gd
 class_name PlayGamesProvider
 extends AuthProvider
 
-func _init():
+func _init() -> void:
 	provider_id = "playgames.google.com"
 	should_exchange = false
